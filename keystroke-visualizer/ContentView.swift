d3e8779148a2b02bb363279effc0d829ef083c3e
unframed-ankha <ankhbayar.enkhtaivan@work.unframed.hk>
2024-08-25 00:49:47 +0800

    init build

---
+++ keystroke-visualizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  keystroke-visualizer
+//
+//  Created by Ankhbayar Enkhtaivan on 8/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ keystroke-visualizer/keystroke_visualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  keystroke_visualizerApp.swift
+//  keystroke-visualizer
+//
+//  Created by Ankhbayar Enkhtaivan on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct keystroke_visualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
